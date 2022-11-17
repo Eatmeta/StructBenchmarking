a5513c8cb97aceb0129b148fb347d8c272a35110
Eatmeta <metagroul@gmail.com>
2022-11-17 18:38:54 +0300

    Implement methods for benchmarking speed creating arrays of classes and arrays of structs. Implement methods for benchmarking speed passing to a method arrays of classes and arrays of structs

--- ExperimentsTask.cs
+++ ExperimentsTask.cs
@@ -2,37 +2,109 @@ using System.Collections.Generic;
 
 namespace StructBenchmarking
 {
+    public interface IExperimentsFactory
+    {
+        IClass CreateClassFactory();
+        IStruct CreateStructFactory();
+    }
+
+    public class ArrayCreationExperimentsFactory : IExperimentsFactory
+    {
+        public IClass CreateClassFactory() => new ArrayCreationForClass();
+        public IStruct CreateStructFactory() => new ArrayCreationForStruct();
+    }
+
+    public class MethodCallExperimentsFactory : IExperimentsFactory
+    {
+        public IClass CreateClassFactory() => new MethodCallForClass();
+        public IStruct CreateStructFactory() => new MethodCallForStruct();
+    }
+
+    public interface IClass
+    {
+        List<ExperimentResult> DoExperimentsForClass(IBenchmark benchmark, int repetitionsCount);
+    }
+
+    public class ArrayCreationForClass : IClass
+    {
+        public List<ExperimentResult> DoExperimentsForClass(IBenchmark benchmark, int repetitionsCount)
+        {
+            var results = new List<ExperimentResult>();
+            for (var i = 16; i <= 512; i *= 2)
+                results.Add(
+                    new ExperimentResult(i,
+                    benchmark.MeasureDurationInMs(new ClassArrayCreationTask(i), repetitionsCount)));
+            
+            return results;
+        }
+    }
+
+    public class MethodCallForClass : IClass
+    {
+        public List<ExperimentResult> DoExperimentsForClass(IBenchmark benchmark, int repetitionsCount)
+        {
+            var results = new List<ExperimentResult>();
+            for (var i = 16; i <= 512; i *= 2)
+                results.Add(new ExperimentResult(i,
+                    benchmark.MeasureDurationInMs(new MethodCallWithClassArgumentTask(i), repetitionsCount)));
+            
+            return results;
+        }
+    }
+
+    public interface IStruct
+    {
+        List<ExperimentResult> DoExperimentsForStruct(IBenchmark benchmark, int repetitionsCount);
+    }
+
+    public class ArrayCreationForStruct : IStruct
+    {
+        public List<ExperimentResult> DoExperimentsForStruct(IBenchmark benchmark, int repetitionsCount)
+        {
+            var results = new List<ExperimentResult>();
+            for (var i = 16; i <= 512; i *= 2)
+                results.Add(new ExperimentResult(i,
+                    benchmark.MeasureDurationInMs(new StructArrayCreationTask(i), repetitionsCount)));
+            
+            return results;
+        }
+    }
+
+    public class MethodCallForStruct : IStruct
+    {
+        public List<ExperimentResult> DoExperimentsForStruct(IBenchmark benchmark, int repetitionsCount)
+        {
+            var results = new List<ExperimentResult>();
+            for (var i = 16; i <= 512; i *= 2)
+                results.Add(new ExperimentResult(i,
+                    benchmark.MeasureDurationInMs(new MethodCallWithStructArgumentTask(i), repetitionsCount)));
+            
+            return results;
+        }
+    }
+
     public class Experiments
     {
-        public static ChartData BuildChartDataForArrayCreation(
-            IBenchmark benchmark, int repetitionsCount)
-        {
-            var classesTimes = new List<ExperimentResult>();
-            var structuresTimes = new List<ExperimentResult>();
-            
-            //...
+        private static readonly IExperimentsFactory ArrayCreationFactory = new ArrayCreationExperimentsFactory();
+        private static readonly IExperimentsFactory MethodCallFactory = new MethodCallExperimentsFactory();
 
+        public static ChartData BuildChartDataForArrayCreation(IBenchmark benchmark, int repetitionsCount)
+        {
             return new ChartData
             {
                 Title = "Create array",
-                ClassPoints = classesTimes,
-                StructPoints = structuresTimes,
+                ClassPoints = ArrayCreationFactory.CreateClassFactory().DoExperimentsForClass(benchmark, repetitionsCount),
+                StructPoints = ArrayCreationFactory.CreateStructFactory().DoExperimentsForStruct(benchmark, repetitionsCount)
             };
         }
 
-        public static ChartData BuildChartDataForMethodCall(
-            IBenchmark benchmark, int repetitionsCount)
+        public static ChartData BuildChartDataForMethodCall(IBenchmark benchmark, int repetitionsCount)
         {
-            var classesTimes = new List<ExperimentResult>();
-            var structuresTimes = new List<ExperimentResult>();
-            
-            //...
-
             return new ChartData
             {
                 Title = "Call method with argument",
-                ClassPoints = classesTimes,
-                StructPoints = structuresTimes,
+                ClassPoints = MethodCallFactory.CreateClassFactory().DoExperimentsForClass(benchmark, repetitionsCount),
+                StructPoints = MethodCallFactory.CreateStructFactory().DoExperimentsForStruct(benchmark, repetitionsCount)
             };
         }
     }
